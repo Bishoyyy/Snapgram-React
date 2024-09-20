7b6891b757f3640d3822c804e888c157180bac56
Bishoy Bassem <bishoybassemsamir@hotmail.com>
2024-09-20 22:21:38 +0300

    Uploaded the project files.

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import AuthProvider from "./context/AuthContext.tsx";
+import { QueryProvider } from "./lib/react-query/QueryProvider.tsx";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <QueryProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
